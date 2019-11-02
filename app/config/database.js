348c656318eb1cf8feaff90216ac5a4d861c273e
Victor Lessa <victordsgnr@gmail.com>
2019-11-02 00:13:42 -0300

    config database from production

--- app/config/database.js
+++ app/config/database.js
@@ -1,29 +1,31 @@
 module.exports = {
   "development": {
-  dialect: "postgres",
-  host: "tuffi.db.elephantsql.com",
-  username: "zalgovvb",
-  password: "Hzy1d3GUnduNAPSarK1Fn91RF10a14Lo",
-  database: "zalgovvb",
-  define: {
-    timestamps: true, // toda tabela deve conter um created_at e um updated_at
-    underscored: true // usa o formato snake_case
-  }
+    dialect: "postgres",
+    host: "tuffi.db.elephantsql.com",
+    username: "zalgovvb",
+    password: "Hzy1d3GUnduNAPSarK1Fn91RF10a14Lo",
+    database: "zalgovvb",
+    define: {
+      timestamps: true, // toda tabela deve conter um created_at e um updated_at
+      underscored: true // usa o formato snake_case
+    }
   },
   "test": {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "postgres",
-    "operatorsAliases": false
+    "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "postgres",
-    "operatorsAliases": false
+    dialect: "postgres",
+    host: "tuffi.db.elephantsql.com",
+    username: "zalgovvb",
+    password: "Hzy1d3GUnduNAPSarK1Fn91RF10a14Lo",
+    database: "zalgovvb",
+    define: {
+      timestamps: true, // toda tabela deve conter um created_at e um updated_at
+      underscored: true // usa o formato snake_case
+    }
   }
 }
