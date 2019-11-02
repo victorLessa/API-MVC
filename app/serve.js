c0598d130dbea9c4c9586019cdf223e8fe0ab3fc
Victor Lessa <victordsgnr@gmail.com>
2019-11-02 00:19:18 -0300

    process port

--- app/serve.js
+++ app/serve.js
@@ -6,6 +6,6 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(route);
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Porta 3000");
 });
