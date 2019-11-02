3df234f58c624ff40b93a7fac616fa64413da9e9
Victor Lessa <victordsgnr@gmail.com>
2019-11-01 23:40:25 -0300

    ecosystem config pm2 from heroku

---
+++ src/ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps : [{
+      name: "app",
+      script: "./serve.js",
+      instances: "max",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      }
+    }]
+  }
+  
