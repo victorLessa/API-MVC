25f51707a215ea3c9c3c30221d9ff37bfa3abe77
Victor Lessa <victordsgnr@gmail.com>
2019-11-01 23:57:52 -0300

    app/serve

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
-      name: "./app",
-      script: "./serve.js",
+      name: "app",
+      script: "./app/serve.js",
       instances: "max",
       env: {
         NODE_ENV: "development",
